bd1f60ed1d3a99bfdebf2bd8c3d18d4be04fa93d
daniel.ionut <daniel.ionu.vicol@gmail.com>
2021-12-18 21:14:31 +0200

    updated log4j use localhost removed unused lib removed script

--- src/main/resources/examples/index.php
+++ src/main/resources/examples/index.php
@@ -7,7 +7,7 @@
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
       <meta name="msapplication-TileColor" content="#da532c">
       <meta name="theme-color" content="#ffffff">
-      <meta http-equiv="refresh" content="0; url=http://www.mtheory7.com:17071/status" />
+      <meta http-equiv="refresh" content="0; url=http://localhost:8989/status" />
       <style>
          body {
          color: #F7931A;
